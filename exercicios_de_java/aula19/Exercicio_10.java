cac03a6c8aef7e4d34cc3ae9d0d8a24cb9a1f04f
Breno santos <Brenomundo263@gmail.com>
2024-09-27 06:26:14 -0300

    adicionado novos exercicios

---
+++ exercicios_de_java/aula19/Exercicio_10.java
@@ -0,0 +1,10 @@
+package exercicios_de_java.aula19;
+
+public class Exercicio_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ exercicios_de_java/aula19/Exercicio_7.java
@@ -0,0 +1,32 @@
+/*Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
+um vetor C, onde cada elemento de C é a subtração dos respectivos
+elementos em A e B, ou seja:
+C[i] = A[i] – B[i].*/
+
+package exercicios_de_java.aula19;
+
+public class Exercicio_7 {
+
+	public static void main(String[] args) {
+		int vetorA []= {1,2,3,4,5,6,7,8,9,10};
+		int vetorB []= {1,2,3,4,5,6,7,8,9,10};
+		
+		int vetorC [] = new int [vetorA.length];
+		
+		for(int i = 0; i<vetorC.length; i++) {
+			vetorC[i] = vetorA[i] - vetorB[i];
+		}
+		
+		for(int i = 0; i<vetorA.length; i++) {
+			System.out.println("vetor A:  posição = %d, valor = %d".formatted(i+1, vetorA[i] ));
+		}
+		for(int i = 0; i<vetorB.length; i++) {
+			System.out.println("vetor B:  posição = %d, valor = %d".formatted(i+1, vetorB[i] ));
+		}
+		for(int i = 0; i<vetorC.length; i++) {
+			System.out.println("vetor C:  posição = %d, valor = %d".formatted(i+1, vetorC[i] ));
+		}
+
+	}
+
+}
