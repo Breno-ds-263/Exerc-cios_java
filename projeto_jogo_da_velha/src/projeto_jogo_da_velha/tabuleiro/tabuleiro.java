2d2c381421726d31ca23b8b3924d206330d7e79e
Breno santos <Brenomundo263@gmail.com>
2024-10-01 21:10:56 -0300

    Adicionoado o exercicio do jogo da velha

---
+++ projeto_jogo_da_velha/src/projeto_jogo_da_velha/tabuleiro/tabuleiro.java
@@ -0,0 +1,10 @@
+package projeto_jogo_da_velha.tabuleiro;
+
+public class tabuleiro {
+	public static char [][] tabuleiro = {
+		    {' ', ' ', ' '},
+		    {' ', ' ', ' '},
+		    {' ', ' ', ' '}
+		};
+
+}
